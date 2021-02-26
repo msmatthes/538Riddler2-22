58760231b8b3b3a41470832f735744c804b41bb4
msmatthes <79430285+msmatthes@users.noreply.github.com>
2021-02-26 09:56:43 -0500

    Update 538Riddler.js

--- 538Riddler.js
+++ 538Riddler.js
@@ -3,25 +3,25 @@
 //recursiveSearch(rightProd: number[], botProd: number[], i: number, fNumbers: number[], rLen: number, bLen: number): null
 function recursiveSearch(rightProd, botProd, i, fNumbers, rLen, bLen){
   for(let x = Math.pow(10, bLen-1); x < Math.pow(10, bLen); ++x){
-    let tArray = convertNumb(x);
-    if(tArray[bLen] === rightProd[i]){
-      fNumbers.push(tArray);
-      if(i !== rLen-1){
-        ++i;
-        recursiveSearch(rightProd, botProd, i, fNumbers, rLen, bLen);
-        fNumbers.pop();
-        --i;
+    let tArray = convertNumb(x); //Sets tArray (temporary array) to the array returned by convertNumb.
+    if(tArray[bLen] === rightProd[i]){ //Checks the sum of the number digits in tArray against the "product" number of the row
+      fNumbers.push(tArray); //If the product matches, add the array to fNumbers
+      if(i !== rLen-1){ //If we are not iterating on the last row
+        ++i; //increase i, which counts the rows, by 1
+        recursiveSearch(rightProd, botProd, i, fNumbers, rLen, bLen); //Run recursive search on the next row
+        fNumbers.pop(); //Get rid of the previously tested last row
+        --i;//Decrease i, so that the program knows we are on the correct row
       }
-      else{
+      else{ //If we are on the last row
         let t = 1;
-        for(let j = 0; j < bLen; ++j){
+        for(let j = 0; j < bLen; ++j){ //These two for loops iterate through the array fNumbers, and find the product of the columns, and makes sure it matches the required product
           let sum = 1;
           for(let k = 0; k < rLen; ++k){
             sum *= fNumbers[k][j];
           }
           t = (sum === botProd[j]) ? t : 0;
         }
-        if(t === 1){
+        if(t === 1){ //If all comumns match, print the solution
           console.log(fNumbers);
         }
         fNumbers.pop();
@@ -30,6 +30,7 @@ function recursiveSearch(rightProd, botProd, i, fNumbers, rLen, bLen){
   }
 }
 
+//This function takes an integer, and returns an array with the format [digit1, digit2,..., Last Digit, Product Of Digits]
 //convertNumb(p: Number): number[]
 function convertNumb(p){
   let fin = [];
